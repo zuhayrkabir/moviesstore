5bb27642842cb6ccbc1caa73588904fdb71b6901
zuhayrkabir <zuhayrkabir1@gmail.com>
2026-01-31 16:18:36 -0500

    switched to SQLite

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -80,14 +80,20 @@ WSGI_APPLICATION = "moviesstore.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'moviesstore',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'NAME': 'moviesstore',
+    #    'USER': 'root',
+    #    'PASSWORD': '',
+    #    'HOST': 'localhost',
+    #    'PORT': '3306',
+    #}
+
+      'default': {
+          'ENGINE': 'django.db.backends.sqlite3',
+          'NAME': BASE_DIR / 'db.sqlite3',
+      }
+
 }
 
 
